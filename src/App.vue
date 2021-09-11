508b6ec33cf2ca860f09e528fbc1c68a57599cc3
DaehyeonKim <Daehyeonoff@gmail.com>
2021-09-12 00:05:20 +0900

    20210912

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-
     <div class="clock">
-        <h1 class="clockText">{{hours}}:{{minutes}}</h1>
+        <div class="bg">
+            <h2 id="h">{{hours}}</h2>
+        </div>
+        <h2>:</h2>
+        <div class="bg">
+            <h2 id="m">{{minutes}}</h2>
+        </div>
     </div>
-
 </template>
 
 <script>
@@ -52,16 +56,42 @@
         margin-top: 60px;
     }
 
-    .clock {
-
-        width: 20%;
-        height: 50%;
-        background: rgba(14, 48, 201, 0.3);
-        border-radius: 20px;
+    body {
+        background: #ebf5fc;
     }
 
-    .clockText {
-        font-size: 100px;
-        color: aliceblue;
+    .clock {
+        color: #000;
+        font-size: 56px;
+        text-align: center;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0 20px;
+    }
+
+    h2 {
+        color: #85C1E9;
+        padding: 30px;
+    }
+
+    .bg {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 4em;
+        height: 4em;
+        background: inherit;
+        position: relative;
+        border-radius: 50%;
+        box-shadow: inset -2px -2px 5px rgba(255, 255, 255, 1), inset 3px 3px 5px rgba(0, 0, 0, 0.2);
+    }
+
+    #m {
+        margin: 0 10px;
     }
 </style>
